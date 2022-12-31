60cb5c6251013b8a2e55e1b6249a0a06b2447750
je4npw <je4n.pw@gmail.com>
2022-12-31 18:40:52 -0300

    Ajustes para página Help

--- components/help/calltoaction.tsx
+++ components/help/calltoaction.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import {
   Box,
   VStack,
@@ -9,9 +10,20 @@ import {
   Container,
 } from "@chakra-ui/react";
 import {} from "@chakra-ui/react";
-import Link from "next/link";
 
-export default function CallToAction() {
+interface Props {
+  header: string;
+  buttonText: string;
+  buttonLink: string;
+  text?: string;
+}
+
+export default function CallToAction({
+  header,
+  buttonText,
+  buttonLink,
+  text,
+}: Props) {
   return (
     <Box as={Container} maxW="7xl" mt={14} p={4} pb={16}>
       <Grid
@@ -25,22 +37,18 @@ export default function CallToAction() {
         <GridItem colSpan={1}>
           <VStack alignItems="flex-start" spacing="20px">
             <chakra.h2 fontSize="3xl" fontWeight="700">
-              Contamos com sua ajuda!
+              {header}
             </chakra.h2>
-            <Link href="/">
+            <Link href={buttonLink}>
               <Button colorScheme="blue" size="md">
-                Voltar para a Página Principal
+                {buttonText}
               </Button>
             </Link>
           </VStack>
         </GridItem>
         <GridItem>
           <Flex>
-            <chakra.p>
-              Agradecemos pela visita ao nosso Site. Divulgue esse trabalho em
-              suas redes sociais e siga as nossas, que estão listadas no rodapé
-              do site e no menu de navegação. Compartilhe nossos materiais.
-            </chakra.p>
+            <chakra.p>{text}</chakra.p>
           </Flex>
         </GridItem>
       </Grid>
--- pages/help.tsx
+++ pages/help.tsx
@@ -7,8 +7,20 @@ export default function Help() {
     <>
       <Container maxW={"5xl"} pt={28}>
         <HelpHeader />
+        <CallToAction
+          header="Entre em contato agora pelo WhatsApp"
+          buttonText="WPP Pr. Cristofer Nunes"
+          buttonLink="https://wa.me/5547984481453?text=Contato+pelo+site.+Gostaria+de+saber+mais+informa%C3%A7%C3%B5es+sobre+a+Casa+Peniel."
+        />
         <HelpCard />
-        <CallToAction />
+        <CallToAction
+          header="Contamos com sua ajuda!"
+          buttonText="Voltar para a Página Principal"
+          buttonLink="/"
+          text="Agradecemos pela visita ao nosso Site. Divulgue esse trabalho em
+          suas redes sociais e siga as nossas, que estão listadas no rodapé
+          do site e no menu de navegação. Compartilhe nossos materiais."
+        />
       </Container>
     </>
   );
